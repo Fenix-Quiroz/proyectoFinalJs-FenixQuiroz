6b6cf9b4a69c4de8b11ab10e6710ae3d7275b697
Fenix Quiroz <fquirozdybala@gmail.com>
2023-01-24 10:42:06 -0500

    mitad del proyecto

--- js/carrito.js
+++ js/carrito.js
@@ -1,32 +1,34 @@
-// aumento de productos por el usuario
-let btnMinus = document.querySelector(".card-cart__minus");
-let btnPlus = document.querySelector(".card-cart__plus")
-let inputQuantity = document.querySelector(".card-cart__quantity")
+// // aumento de productos por el usuario
+// let btnMinus = document.querySelector(".card-cart__minus");
+// let btnPlus = document.querySelector(".card-cart__plus")
+// let inputQuantity = document.querySelector(".card-cart__quantity")
 
 
-let quantity= 0;
+// let quantity= 0;
 
-btnMinus.addEventListener('click',()=>{
-    quantity--;
-    if (quantity<= 0) {
-        quantity = 0;
-    }
-    inputQuantity.value= quantity;
-})
-btnPlus.addEventListener('click',()=>{
-    quantity++;
-    inputQuantity.value= quantity;
-})
+// //btn aumentar
+// btnMinus.addEventListener('click',()=>{
+//     quantity--;
+//     if (quantity<= 0) {
+//         quantity = 0;
+//     }
+//     inputQuantity.value= quantity;
+// })
+// //btn disminuir
+// btnPlus.addEventListener('click',()=>{
+//     quantity++;
+//     inputQuantity.value= quantity;
+// })
 
-// btn comprar
-let btnComprar= document.querySelector("#card-confirm__btn")
-let btnContent=document.querySelector(".cart__modal")
-btnComprar.addEventListener('click',()=>{
-    btnContent.style= "display: block"
-})
+// // btn comprar
+// let btnComprar= document.querySelector("#card-confirm__btn")
+// let btnContent=document.querySelector(".cart__modal")
+// btnComprar.addEventListener('click',()=>{
+//     btnContent.style= "display: block"
+// })
 
- let btnPagar = document.querySelector("#cart-form__btn")
- btnPagar.addEventListener('click', ()=>{
-    Swal.fire('Producto comprado')
- })
+//  let btnPagar = document.querySelector("#cart-form__btn")
+//  btnPagar.addEventListener('click', ()=>{
+//     Swal.fire('Producto comprado')
+//  })
 
--- js/main.js
+++ js/main.js
@@ -1,53 +1,19 @@
 // buscador
 const search = document.querySelector(".search__fetch");
-const btnSearch = document.querySelector(".btn-search")
-const container= document.querySelector(".products")
+const btnSearch = document.querySelector(".btn-search");
+const container = document.querySelector(".products");
+console.log(container);
 const chekboxsFilter = document.querySelectorAll('input[type="checkbox"]');
 
 
-
-const products =[
-    //vinos semisecos
-    {id: 1 ,tipo:"semiseco", name:"Vino Tabernero Gran Rosé" , img:"img/producto-semiseco1.jpg" , price:"$40"},
-    {id: 2 ,tipo:"semiseco", name:"Vino Tabernero Gran Blanco" , img:"img/producto-semiseco2.png" , price:"$40"},
-    {id: 3 ,tipo:"semiseco", name:"Vino Tabernero Gran Tinto" , img:"img/producto-semiseco3.jpg" , price:"$40"},
-    {id: 4 ,tipo:"semiseco", name:"Vino Tabernero Rosé Gran Selección" , img:"img/producto-semiseco4.jpg" , price:"$40"},
-    {id: 5 ,tipo:"semiseco", name:"Vino Tabernero Tinto País" , img:"img/producto-semiseco5.jpg" , price:"$40"},
-    {id: 6 ,tipo:"semiseco", name:"Vino Tabernero Rosé Tuyo" , img:"img/producto-semiseco6.jpg" , price:"$40"},
-
-    //vinos secos
-    {id: 7 ,tipo:"seco", name:"Vino Gran Tinto Malbec Merlot" , img:"img/producto-seco1.png" , price:"$40"},
-    {id: 8 ,tipo:"seco", name:"Vino Gran Tinto Cabernet Sauvignon" , img:"img/producto-seco2.png" , price:"$40"},
-    {id: 9 ,tipo:"seco", name:"Vino Gran Blanco Fina Reserva" , img:"img/producto-seco3.png" , precio:"$40"},
-
-    //vinos vittoria
-    {id: 10 ,tipo:"vittoria", name:"Vino Vittoria Malbec" , img:"img/producto-vittoria1.png" , price:"$40"},
-    {id: 11 ,tipo:"vittoria", name:"Vino Vittoria Syrah" , img:"img/producto-vittoria2.png" , price:"$40"},
-    {id: 12 ,tipo:"vittoria", name:"Vino Vittoria Cabernet Sauvignon" , img:"img/producto-vittoria3.png" , price:"$40"},
-    {id: 13 ,tipo:"vittoria", name:"Vino Vittoria Rosé de Syrah" , img:"img/producto-vittoria4.png" , price:"$40"},
-    {id: 14 ,tipo:"vittoria", name:"Vino Vittoria Sauvignon Blanc" , img:"img/producto-vittoria5.png" , price:"$40"},
-    {id: 15 ,tipo:"vittoria", name:"Vino Vittoria Chardonnay" , img:"img/producto-vittoria6.png" , price:"$40"},
-
-    //vinos generosos
-    {id: 16 ,tipo:"generosos", name:"Vino El Fraile Oporto Tabernero" , img:"img/producto-generoso1.png" , price:"$40"},
-]
-
-
-// filtra productos
- function filterProducts(filter) {
-    console.log(filter);
-    let filteredOut = products.filter((el) =>{
-        return el.name.includes(filter);
-    })
-    return filteredOut;
- }
 // crear tajeta con html
 function createHtml(arr) {
-    let html;
-    container.innerHTML="";
-    // tarjeta
-    for (const product of arr) {
-        html= `<div class="card-prod">
+  let html;
+  container.innerHTML = "";
+  
+  // tarjeta
+  for (const product of arr) {
+    html = `<div class="card-prod" id="card-product">
         <div class="card-prod__img">
           <a href="productos.html">
             <img
@@ -64,35 +30,119 @@ function createHtml(arr) {
           </button>
         </div>
       </div>`;
-      container.innerHTML += html
-    }
-}
-
-createHtml(products);
-// search
-search.addEventListener('input', ()=>{
-    let filter = filterProducts (search.value)
-    createHtml(filter)
-} )
-
-
-
-
-//filter
-  function filterInputChekbox(filter) {
-      
-      let filterInput = products.filter((el) =>{
-           return el.tipo ==(filter);
-     })
-      return filterInput;
+    container.innerHTML += html;
   }
- chekboxsFilter.forEach(element =>{
-     element.addEventListener('click', (event)=>{
-          let filter = filterInputChekbox(element.value);
-          console.log(element.value);
-          createHtml(filter)
-        
-     })
+}
+// fetch (trear datos de un JSON)
+fetch("./data/data.json")
+  .then((response) => response.json())
+  .then((data) => {
+    // tarjetas de productos
+    createHtml(data);
+    // filtro buscar
+    function filterProducts(filter) {
+      let filteredOut = data.filter((el) => {
+        return el.name.includes(filter);
+      });
+      return filteredOut;
+    }
+    // buscador
+    const cartProduct= document.querySelectorAll(".card-prod")
+    search.addEventListener("input", () => {
+      let filter = filterProducts(search.value);
+      createHtml(filter)
       
- })
+      
+    });
 
+    // filtro p0r checkboxs
+    function filterInputChekbox(filter) {
+      let filterInput = data.filter((el) => {
+        return el.tipo == filter;
+      });
+      return filterInput;
+    }
+    chekboxsFilter.forEach((element) => {
+      element.addEventListener("click", (event) => {
+        
+        let filter = filterInputChekbox(element.value);
+        let prductsTitle = document.querySelector(".products__title");
+        if (element.checked == true) {
+          createHtml(filter);
+          prductsTitle.innerHTML = `VINOS ${element.id}`;
+        } else {
+          createHtml(data);
+          prductsTitle.innerText = "NUESTROS VINO DISPONIBLES";
+        }
+      });
+    });
+
+    //notificacion al agregar al carrito
+    const btnCart = document.querySelectorAll(".card-prod__btn");
+    let cartNotification = document.querySelector(".cart__amount");
+    let cartQuantity = 0;
+    // btn agrgar al carrito
+    btnCart.forEach((element) => {
+      element.addEventListener("click", () => {
+        cartQuantity++;
+        cartNotification.style = "display: block;";
+        cartNotification.innerText = cartQuantity;
+
+        // guardar productos del carrito en local storage
+
+        //localStorage.setItem('product' ,JSON.stringify() )
+      });
+    });
+  });
+
+// agregar productos al carrito
+// let cartProducts= document.querySelector(".main-cart");
+
+// function addTocartProducts() {
+//   products.forEach(product => {
+//     let createCardHtml = `<div class="card-cart">
+//     <div class="card-cart__img">
+//       <img src="${product.img}" />
+//     </div>
+//     <div class="card-cart__info">
+//       <img
+//         class="card-cart__delete"
+//         src="./img/icon-delete.svg"
+//         alt="Icon delete"
+//       />
+//       <p class="card-cart__name">${products.name}</p>
+//       <div class="card-cart__items">
+//         <p class="card-cart__item">Precio:</p>
+//         <p class="card-cart__item">Cantidad:</p>
+//         <p class="card-cart__item">Total:</p>
+//       </div>
+//       <div class="card-cart__dates">
+//         <p class="card-cart__price">${product.price}</p>
+//         <div class="card-cart__quantity-products">
+//           <img
+//             class="card-cart__minus"
+//             src="./img/icon-minus.svg"
+//             alt="Icon minus."
+//           />
+
+//           <input
+//             class="card-cart__quantity"
+//             type="number"
+//             id="quantity-products"
+//             value="0"
+//           />
+
+//           <img
+//             class="card-cart__plus"
+//             src="./img/icon-plus.svg"
+//             alt="Icon plus."
+//           />
+//         </div>
+
+//         <p  class="card-cart__total">$300</p>
+//       </div>
+//     </div>
+//   </div>`
+//       cartProducts += createCardHtml;
+//   });
+// }
